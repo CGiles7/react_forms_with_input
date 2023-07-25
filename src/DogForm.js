16270d9fbeff314fe68f02769d2cc46954d52c47
Callie Giles <anchoredinthecrown@gmail.com>
2023-07-25 02:22:26 -0400

    add form, submit handler

--- src/DogForm.js
+++ src/DogForm.js
@@ -1,5 +1,38 @@
 import React, { useState } from "react";
 
-function DogForm() {}
+function DogForm() {
+  const [name, setName] = useState("");
+  const [breed, setBreed] = useState("");
+  const [age, setAge] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`${name}`, `${breed}`,`${age}`);
+    setName("");
+    setBreed("");
+    setAge("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Breed:
+        <input type="text" value={breed} onChange={(e) => setBreed(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Age:
+        <input type="text" value={age} onChange={(e) => setAge(e.target.value)} />
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
 
 export default DogForm;
